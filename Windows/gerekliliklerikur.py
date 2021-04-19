4e6bacf80562b614630d5033e24d878a053680cd
ilker610 <75069363+ilker610@users.noreply.github.com>
2021-04-19 19:22:21 +0300

    Add files via upload

---
+++ Windows/gerekliliklerikur.py
@@ -0,0 +1,9 @@
+import os
+
+bir = ('pip install PyQt5')
+iki = ('pip install pyqt5-tools')
+üç = ('pip install PyQtWebEngine')
+os.system(bir)
+os.system(iki)
+os.system(üç)
+print('Bileşenler Kuruldu !')
