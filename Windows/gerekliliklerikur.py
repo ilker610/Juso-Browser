e7825f67066db3e244f1e5d5f1dbda19cf67ac80
ilker610 <75069363+ilker610@users.noreply.github.com>
2021-04-20 22:34:22 +0300

    Delete Windows directory

--- Windows/gerekliliklerikur.py
+++
@@ -1,9 +0,0 @@
-import os
-
-bir = ('pip install PyQt5')
-iki = ('pip install pyqt5-tools')
-üç = ('pip install PyQtWebEngine')
-os.system(bir)
-os.system(iki)
-os.system(üç)
-print('Bileşenler Kuruldu !')
